58502c06d00baf1068e26ee00639903c76445998
0Exe <69282787+0Exe@users.noreply.github.com>
2021-02-20 23:01:27 -0300

    Add files via upload

---
+++ iseven.py
@@ -0,0 +1,11 @@
+def even(num:int):
+    if num % 2 != 0:
+        return(False)
+    else:
+        return(True)
+
+async def async_even(num:int):
+    if num % 2 != 0:
+        return(False)
+    else:
+        return(True)
